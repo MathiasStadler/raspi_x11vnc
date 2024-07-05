b9345f3e3af4197884e0dcfb7cd34a0c49d6c1ba
Mathias <email@example.com>
2024-07-05 11:01:42 +0200

    add new file

---
+++ start_x11vnc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function run_vnc_server() {
+    export DISPLAY=:1
+    local passwordArgument='-nopw'
+
+    if [ -n "${VNC_SERVER_PASSWORD}" ]
+    then
+        local passwordFilePath="${HOME}/x11vnc.pass"
+        if ! x11vnc -storepasswd "${VNC_SERVER_PASSWORD}" "${passwordFilePath}"
+        then
+            echo "[ERROR] Failed to store x11vnc password."
+            exit 1
+        fi
+        passwordArgument=-"-rfbauth ${passwordFilePath}"
+        echo "[INFO] The VNC server will ask for a password."
+    else
+        echo "[WARN] The VNC server will NOT ask for a password."
+    fi
+
+    x11vnc -ncache 10 -display ${DISPLAY} -forever "${passwordArgument}"  &
+    # x11vnc -xkb -display ${DISPLAY} -forever ${passwordArgument} &
+    wait $!
+}
+
+run_vnc_server;
